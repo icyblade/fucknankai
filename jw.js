3db77669eb6d4b53ac04af16caf89a2f58d6ea0d
Icyblade Dai <icyblade@users.noreply.github.com>
2015-12-23 10:49:01 +0800

    Create jw.js

---
+++ jw.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         FuckNankaiJW
+// @namespace    http://github.com/icyblade/fucknankai
+// @version      0.1
+// @description  Fuck these noob programmers
+// @author       Icyblade
+// @run-at       document-end
+// @grant        none
+// @include      http://222.30.32.10/*
+// ==/UserScript==
+
+document.getElementsByName("userpwd_text")[0].id="userpwd_text"
